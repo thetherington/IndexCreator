30e6539da86130c4728d75baf6d4064f06458d18
Tom Hetherington <thomas@hetheringtons.org>
2023-03-16 14:22:16 -0400

    init

---
+++ cmd/cli/main.go
@@ -0,0 +1,246 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/chelnak/ysmrr"
+	"github.com/thetherington/IndexCreator/internal/helpers"
+)
+
+const FILE string = "log-syslog-warning-2021.08.22.tar.gz"
+const MODE = 0755
+
+type App struct {
+	Filename   string
+	Index      string
+	FileDate   string
+	IndexDates []time.Time
+	Spinners   []*ysmrr.Spinner
+	wg         sync.WaitGroup
+}
+
+func main() {
+	var app App
+
+	createCmd := flag.NewFlagSet("create", flag.ExitOnError)
+
+	startPtr := createCmd.String("start", "", "YYYY-MM-DD")
+	endPtr := createCmd.String("end", "", "YYYY-MM-DD")
+
+	if len(os.Args) < 2 {
+		fmt.Println("expected 'create' or 'import' subcommands")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "create":
+		createCmd.Parse(os.Args[2:])
+
+		start, valid := ValidDateInput(startPtr)
+		if !valid {
+			fmt.Println("Start value is invalid")
+			os.Exit(1)
+		}
+
+		end, valid := ValidDateInput(endPtr)
+		if !valid {
+			fmt.Println("End value is invalid")
+			os.Exit(1)
+		}
+
+		app.InitDateRanges(start, end)
+
+		if len(createCmd.Args()) < 1 {
+			fmt.Println("No export archive provided")
+			os.Exit(1)
+		}
+
+		// Check if the input file exists
+		r, err := os.Open(createCmd.Args()[0])
+		if err != nil {
+			fmt.Println("Archive file provided does not exist")
+			os.Exit(1)
+		}
+		r.Close()
+
+		app.Filename = r.Name()
+
+		// parse the file name to get the index name and date from the filename
+		re := regexp.MustCompile(`(\.\/)*(.*)(\d{4}\.\d{2}\.\d{2})`)
+
+		for _, match := range re.FindAllStringSubmatch(r.Name(), -1) {
+			app.Index = strings.TrimSuffix(match[2], "-")
+			app.FileDate = match[3]
+		}
+
+	default:
+		fmt.Println("expected 'create' or 'import' subcommands")
+		os.Exit(1)
+	}
+
+	// Create spin group
+	sm := ysmrr.NewSpinnerManager()
+
+	for i := 0; i < len(app.IndexDates); i++ {
+		s := sm.AddSpinner(fmt.Sprintf("%s -- Extracting...", app.IndexDates[i].Format("2006.01.02")))
+		app.Spinners = append(app.Spinners, s)
+	}
+
+	sm.Start()
+
+	// iterate through each date and generate the import in a go routine
+	for x, d := range app.IndexDates {
+		app.wg.Add(1)
+
+		go app.GenerateIndex(d, app.Spinners[x])
+	}
+
+	// wait for all to complete
+	app.wg.Wait()
+
+	sm.Stop()
+
+}
+
+func (app *App) GenerateIndex(dt time.Time, s *ysmrr.Spinner) {
+	defer app.wg.Done()
+
+	new_date := dt.Format("2006.01.02")
+
+	r, err := os.Open(fmt.Sprintf("./%s", app.Filename))
+	if err != nil {
+		s.UpdateMessage(fmt.Sprintf("%s -- %s", new_date, err.Error()))
+		s.Error()
+		return
+	}
+	defer r.Close()
+
+	path := filepath.Join(app.Index, new_date)
+
+	// Make directory based on date and Untar the reference tar.gz file
+	err = os.MkdirAll(path, MODE)
+	if err != nil {
+		s.UpdateMessage(fmt.Sprintf("%s -- %s", new_date, err.Error()))
+		s.Error()
+		return
+	}
+
+	err = helpers.Untar(path, r)
+	if err != nil {
+		s.UpdateMessage(fmt.Sprintf("%s -- %s", new_date, err.Error()))
+		s.Error()
+		return
+	}
+
+	// replace Index name to new Index name
+	old_index_pattern := fmt.Sprintf("%s-%s", app.Index, app.FileDate)
+	new_index_pattern := fmt.Sprintf("%s-%s", app.Index, new_date)
+
+	s.UpdateMessage(fmt.Sprintf("%s -- Replacing (%s with %s)...", new_date, old_index_pattern, new_index_pattern))
+
+	err = helpers.SedReplace(path, old_index_pattern, new_index_pattern)
+	if err != nil {
+		s.UpdateMessage(fmt.Sprintf("%s -- %s", new_date, err.Error()))
+		s.Error()
+		return
+	}
+
+	// replace @timestamp with new date value
+	old_date_pattern := strings.ReplaceAll(app.FileDate, ".", "-")
+	new_date_pattern := strings.ReplaceAll(new_date, ".", "-")
+
+	s.UpdateMessage(fmt.Sprintf("%s -- Replacing (%s with %s)...", new_date, old_date_pattern, new_date_pattern))
+
+	err = helpers.SedReplace(path, old_date_pattern, new_date_pattern)
+	if err != nil {
+		s.UpdateMessage(fmt.Sprintf("%s -- %s", new_date, err.Error()))
+		s.Error()
+		return
+	}
+
+	// Create new tar.gz file
+	s.UpdateMessage(fmt.Sprintf("%s -- Taring...", new_date))
+
+	w, err := os.Create(fmt.Sprintf("./%s/%s-%s.tar.gz", app.Index, app.Index, new_date))
+	if err != nil {
+		s.UpdateMessage(fmt.Sprintf("%s -- %s", new_date, err.Error()))
+		s.Error()
+		return
+	}
+
+	err = helpers.Tar(path, w)
+	if err != nil {
+		s.UpdateMessage(fmt.Sprintf("%s -- %s", new_date, err.Error()))
+		s.Error()
+		return
+	}
+
+	// Delete the work dir
+	s.UpdateMessage(fmt.Sprintf("%s -- Cleaning...", new_date))
+
+	err = os.RemoveAll(path)
+	if err != nil {
+		s.UpdateMessage(fmt.Sprintf("%s -- %s", new_date, err.Error()))
+		s.Error()
+		return
+	}
+
+	s.UpdateMessage(fmt.Sprintf("%s -- Complete", new_date))
+	s.Complete()
+}
+
+func (app *App) InitDateRanges(start, end time.Time) error {
+	y, m, d := start.Date()
+	start = time.Date(y, m, d, 0, 0, 0, 0, time.UTC)
+
+	y, m, d = end.Date()
+	end = time.Date(y, m, d, 0, 0, 0, 0, time.UTC)
+
+	for rd := helpers.DateRange(start, end); ; {
+		date := rd()
+		if date.IsZero() {
+			break
+		}
+		app.IndexDates = append(app.IndexDates, date)
+	}
+
+	return nil
+}
+
+func (app *App) Cleanup() error {
+	err := os.RemoveAll(app.Index)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ValidDateInput(date *string) (time.Time, bool) {
+	re := regexp.MustCompile(`\d{4}\-\d{2}\-\d{2}`)
+
+	if re.MatchString(*date) {
+
+		d := strings.Split(*date, "-")
+
+		year, _ := strconv.Atoi(d[0])
+		month, _ := strconv.Atoi(d[1])
+		day, _ := strconv.Atoi(d[2])
+
+		if month > 12 || day > 32 {
+			return time.Now(), false
+		}
+
+		return time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC), true
+	}
+
+	return time.Now(), false
+}
---
+++ internal/helpers/helpers.go
@@ -0,0 +1,184 @@
+package helpers
+
+import (
+	"archive/tar"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+)
+
+// Untar takes a destination path and a reader; a tar reader loops over the tarfile
+// creating the file structure at 'dst' along the way, and writing any files
+func Untar(dst string, r io.Reader) error {
+	gzr, err := gzip.NewReader(r)
+	if err != nil {
+		return err
+	}
+	defer gzr.Close()
+
+	tr := tar.NewReader(gzr)
+
+	re := regexp.MustCompile(`\d{4}\.\d{2}\.\d{2}`)
+
+	for {
+		header, err := tr.Next()
+
+		switch {
+		// if no more file are found return
+		case err == io.EOF:
+			return nil
+
+		// return any other error
+		case err != nil:
+			return err
+
+		// if the header is nil, just skip it (not sure how this happen)
+		case header == nil:
+			continue
+
+		}
+
+		// find the date value in the file name and replace it with the value in the paths
+		dir := re.FindStringSubmatch(dst)[0]
+		newHeaderName := re.ReplaceAll([]byte(header.Name), []byte(dir))
+
+		// the target location where the dir/file should be created
+		target := filepath.Join(dst, string(newHeaderName))
+
+		// check the file type
+		switch header.Typeflag {
+
+		// if its a dir and it doesn't exist create it
+		case tar.TypeDir:
+			if _, err := os.Stat(target); err != nil {
+				if err := os.MkdirAll(target, 0755); err != nil {
+					return err
+				}
+			}
+
+		// if it's a file create it
+		case tar.TypeReg:
+			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
+			if err != nil {
+				return err
+			}
+
+			// copy over contents
+			if _, err := io.Copy(f, tr); err != nil {
+				return err
+			}
+
+			// manually close here after each file operation; defering would cause each file close
+			// to wait until all operations have completed.
+			f.Close()
+		}
+
+	}
+}
+
+// Tar takes a source and variable writers and walks 'source' writing each file
+// found to the tar writer; the purpose for accepting multiple writers is to allow
+// for multiple outputs (for example a file, or md5 hash)
+func Tar(src string, writers ...io.Writer) error {
+
+	// ensure the src actually exists before trying to tar it
+	if _, err := os.Stat(src); err != nil {
+		return fmt.Errorf("unable to tar files - %v", err.Error())
+	}
+
+	mw := io.MultiWriter(writers...)
+
+	gzw := gzip.NewWriter(mw)
+	defer gzw.Close()
+
+	tw := tar.NewWriter(gzw)
+	defer tw.Close()
+
+	// walk path
+	return filepath.Walk(src, func(file string, fi os.FileInfo, err error) error {
+
+		// return on any error
+		if err != nil {
+			return err
+		}
+
+		// return on non-regular files (thanks to [kumo](https://medium.com/@komuw/just-like-you-did-fbdd7df829d3) for this suggested update)
+		if !fi.Mode().IsRegular() {
+			return nil
+		}
+
+		// create a new dir/file header
+		header, err := tar.FileInfoHeader(fi, fi.Name())
+		if err != nil {
+			return err
+		}
+
+		// update the name to correctly reflect the desired destination when untaring
+		header.Name = strings.TrimPrefix(strings.Replace(file, src, "", -1), string(filepath.Separator))
+
+		// write the header
+		if err := tw.WriteHeader(header); err != nil {
+			return err
+		}
+
+		// open files for taring
+		f, err := os.Open(file)
+		if err != nil {
+			return err
+		}
+
+		// copy file data into tar writer
+		if _, err := io.Copy(tw, f); err != nil {
+			return err
+		}
+
+		// manually close here after each file operation; defering would cause each file close
+		// to wait until all operations have completed.
+		f.Close()
+
+		return nil
+	})
+}
+
+func DateRange(start, end time.Time) func() time.Time {
+	y, m, d := start.Date()
+	start = time.Date(y, m, d, 0, 0, 0, 0, time.UTC)
+
+	y, m, d = end.Date()
+	end = time.Date(y, m, d, 0, 0, 0, 0, time.UTC)
+
+	return func() time.Time {
+		if start.After(end) {
+			return time.Time{}
+		}
+		date := start
+		start = start.AddDate(0, 0, 1)
+		return date
+	}
+}
+
+func SedReplace(path, original, new string) error {
+	files, err := os.ReadDir(path)
+	if err != nil {
+		return err
+	}
+
+	sed_replace := fmt.Sprintf("s/%s/%s/g", original, new)
+
+	for _, item := range files {
+		command := exec.Command("sed", "-i", sed_replace, filepath.Join(path, item.Name()))
+
+		err := command.Run()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
